fdff2d85ce52d7f250a9bcd2cd2e21561802564c
Al <albinma@gmail.com>
2023-03-02 01:56:57 -0500

    Test PR (#1)
    * Test PR
    
    * Update actions
    
    * Change workflow name
    
    * Set CI to true in action env
    
    * Fix playwright test
    
    * Set reuseExistingServer to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ const config: PlaywrightTestConfig = {
     command: 'yarn dev',
     url: baseURL,
     timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 
   use: {
