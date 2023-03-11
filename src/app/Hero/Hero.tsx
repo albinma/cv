fdfeebb4bbb6f71e31ebd8ff4f08477dff9cbc28
Albin Ma <albinma@gmail.com>
2023-03-10 23:48:18 -0500

    Add side menu

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -1,17 +1,14 @@
 export default function Hero() {
   return (
     <div className="flex h-screen flex-col justify-center">
-      <h2 className="text-4xl font-bold text-slate-500">
+      <h2 className="text-xl font-bold text-slate-500 md:text-4xl">
         Hey, I&apos;m <span className="text-pink-500">Albin Ma</span> and...
       </h2>
-      <h1 className="mt-4 text-8xl font-bold text-slate-900">
+      <h1 className="mt-4 text-5xl font-bold text-slate-900 md:text-8xl">
         I&apos;m a Software Engineer
       </h1>
-      <p className="mt-8 text-2xl text-slate-500">
-        I have a passion in building things for the web.
-      </p>
-      <p className="mt-4 text-xl text-slate-400">
-        I&apos;m currently @{' '}
+      <p className="text-md mt-16 text-slate-400 md:text-2xl">
+        I&apos;m currently working hard @{' '}
         <a
           href="https://www.touchbistro.com/"
           className="font-semibold"
@@ -20,8 +17,7 @@ export default function Hero() {
           <span className="text-tb-primary">Touch</span>
           <span className="text-tb-secondary">Bistro</span>
         </a>{' '}
-        helping with empowering restaurants with their innovative POS and cloud
-        management system.
+        building features that empower restaurants.
       </p>
     </div>
   );
--- src/app/Nav/Nav.tsx
+++ src/app/Nav/Nav.tsx
@@ -1,18 +1,26 @@
 'use client';
 
+import cn from 'classnames';
 import Link from 'next/link';
 import { RefObject, useEffect, useRef, useState } from 'react';
 import * as Icon from 'react-feather';
 
 export default function Nav() {
+  const menuItems = [
+    { label: 'Home', href: '#', icon: <Icon.Home className="h-6 w-6" /> },
+    { label: 'About', href: '#', icon: <Icon.User className="h-6 w-6" /> },
+    { label: 'Work', href: '#', icon: <Icon.Briefcase className="h-6 w-6" /> },
+    {
+      label: 'Projects',
+      href: '#',
+      icon: <Icon.Codesandbox className="h-6 w-6" />,
+    },
+    { label: 'Contact', href: '#', icon: <Icon.Mail className="h-6 w-6" /> },
+  ];
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const sideMenuButton = useRef(null);
   const sideMenu = useRef(null);
-
-  const menuOnClick = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   useEffect(() => {
     console.log('🚀 ~ file: Nav.tsx:67 ~ Nav ~ isMenuOpen:', isMenuOpen);
@@ -46,39 +54,38 @@ export default function Nav() {
 
   return (
     <>
-      <header className="sticky top-0 w-full border-b-2 bg-slate-100">
-        <div className="flex flex-nowrap items-center justify-between py-4 px-8">
+      <header className="sticky top-0 z-50 w-full border-b-2 bg-slate-100">
+        <div className="flex flex-nowrap items-center justify-between py-4 px-4 md:px-8">
           <div className="grow">
             <Link
               href="/"
-              className="border-4 border-slate-900 px-2 text-3xl font-bold text-slate-900"
+              className="border-4 border-slate-900 px-2 text-xl font-bold text-slate-900 md:text-3xl"
             >
               AM
             </Link>
           </div>
           <div className="flex items-center md:hidden">
-            <button>
-              <Icon.Menu className="h-6 w-6" />
+            <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              {isMenuOpen ? (
+                <Icon.X className="h-6 w-6" />
+              ) : (
+                <Icon.Menu className="h-6 w-6" />
+              )}
             </button>
           </div>
           <div className="hidden flex-nowrap items-center justify-between md:flex">
             <nav className="px-6">
               <ul className="mx-auto flex flex-row items-center justify-between space-x-6">
-                <li>
-                  <a href="#" className="hover:underline">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Work
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Projects
-                  </a>
-                </li>
+                {menuItems.map((item) => (
+                  <li key={item.label}>
+                    <a
+                      href={item.href}
+                      className="text-slate-900 hover:underline"
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </nav>
             <button className="flex flex-row items-center justify-between rounded-md bg-pink-500 py-2 px-3 text-slate-100">
@@ -88,6 +95,32 @@ export default function Nav() {
           </div>
         </div>
       </header>
+      <aside
+        className={cn(
+          'fixed top-0 right-0 z-40 flex h-screen w-full flex-col items-end justify-center border-l-2 bg-slate-100 bg-opacity-30 backdrop-blur-lg backdrop-filter transition-transform duration-75 ease-in-out md:hidden',
+          !isMenuOpen && 'translate-x-full',
+        )}
+      >
+        <nav className="flex h-full w-72 flex-col items-center justify-center border-l-2 bg-slate-100 bg-opacity-100 p-8">
+          <ul className=" space-y-8 text-2xl text-slate-700">
+            {menuItems.map((item) => (
+              <li key={item.label} className="flex flex-row items-center">
+                <span className="mr-2">{item.icon}</span>
+                <a href={item.href} className="hover:underline">
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <button className="mt-16 flex w-48 flex-row items-center justify-between rounded-md bg-pink-500 py-2 px-3 text-slate-100">
+            <div className="mx-auto flex flex-row items-center justify-between">
+              <Icon.Download className="mr-2 h-4 w-4" />
+              <span>Resume</span>
+            </div>
+          </button>
+        </nav>
+      </aside>
     </>
   );
 }
