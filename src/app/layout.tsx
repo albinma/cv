05c5dcd0f0677cecfd4e3f07d5e326d983cb7b3b
Albin Ma <albinma@gmail.com>
2023-06-20 17:57:29 -0400

    Skip rendering gtag scripts if no var value

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,23 +13,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+
   return (
     <html lang="en">
-      <Script id="gtm" strategy="afterInteractive">
-        {`
+      {GTM_ID && (
+        <Script id="gtm" strategy="afterInteractive">
+          {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${GTM_ID}');
         `}
-      </Script>
+        </Script>
+      )}
       <body className="bg-slate-50">
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }}
-        />
+        {GTM_ID && (
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
+        )}
         <Nav />
         {children}
       </body>
