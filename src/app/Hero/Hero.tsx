319260f967fdeb515d9bd7588c123241c47f9f8f
Albin Ma <albinma@gmail.com>
2023-06-20 16:35:28 -0400

    Add favicons

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero(): JSX.Element {
         <h1 className="mt-8 text-5xl font-bold text-slate-900 md:text-8xl">
           I&apos;m a Software Engineer
         </h1>
-        <p className="text-md mt-24 text-slate-400 md:text-2xl">
+        <p className="text-md mt-24 text-slate-500 md:text-2xl">
           I&apos;m currently working hard{' '}
           <a
             href="https://www.touchbistro.com/"
